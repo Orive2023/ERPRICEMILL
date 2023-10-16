2fe96a8411e4810dca8490acd04f412d99da17d7
OriveSolutions <138654609+OriveSolutions@users.noreply.github.com>
2023-10-16 18:33:19 +0530

    Sourav changes

--- Supplier/src/main/java/com/orive/Supplier/Controller/SupplierController.java
+++ Supplier/src/main/java/com/orive/Supplier/Controller/SupplierController.java
@@ -18,65 +18,72 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.orive.Supplier.Dto.SupplierDto;
-import com.orive.Supplier.Service.SupplierService;
+import com.orive.Supplier.Dto.CorporateSupplierDto;
+import com.orive.Supplier.Dto.IndividualSupplierDto;
+import com.orive.Supplier.Service.CorporateSupplierService;
+import com.orive.Supplier.Service.IndividualSupplierService;
 
 @RestController
-@RequestMapping(value = "/api/suppliers")
-@CrossOrigin("http://localhost:3000")
+@RequestMapping(value = "/suppliers")
+@CrossOrigin("*")
 public class SupplierController {
 	
 	private Logger logger=LoggerFactory.getLogger(SupplierController.class);
 
 	@Autowired
-	private SupplierService supplierService;
+	private IndividualSupplierService individualSupplierService;
 	
+	@Autowired
+	private CorporateSupplierService corporateSupplierService;
+	
+	//API FOR INDIVIDUAL SUPPLIER
 	// Create a new supplier
     @PostMapping("/create/supplier")
-    public ResponseEntity<SupplierDto> createSupplier(@RequestBody SupplierDto supplierDTO) {
-        SupplierDto createdSupplier = supplierService.createSupplier(supplierDTO);
+    public ResponseEntity<IndividualSupplierDto> createSupplier(@RequestBody IndividualSupplierDto supplierDTO) {
+        IndividualSupplierDto createdSupplier = individualSupplierService.createSupplier(supplierDTO);
         logger.info("Created supplier with name: {}", createdSupplier.getSupplierName());
         return new ResponseEntity<>(createdSupplier, HttpStatus.CREATED);
     }
 
     // Get all suppliers
+    
     @GetMapping("/get/suppliers")
-    public ResponseEntity<List<SupplierDto>> getAllSuppliers() {
-        List<SupplierDto> suppliers = supplierService.getAllSuppliers();
+    public ResponseEntity<List<IndividualSupplierDto>> getAllSuppliers() {
+        List<IndividualSupplierDto> suppliers = individualSupplierService.getAllSuppliers();
         logger.info("Retrieved {} suppliers from the database", suppliers.size());
         return new ResponseEntity<>(suppliers, HttpStatus.OK);
     }
 
     // Get supplier by ID
     @GetMapping("/get/{supplierId}")
-    public ResponseEntity<SupplierDto> getSupplierById(@PathVariable Long supplierId) {
-        Optional<SupplierDto> supplier = supplierService.getSupplierById(supplierId);
+    public ResponseEntity<IndividualSupplierDto> getSupplierById(@PathVariable Long individualSupplierId) {
+        Optional<IndividualSupplierDto> supplier = individualSupplierService.getSupplierById(individualSupplierId);
         if (supplier.isPresent()) {
-            logger.info("Retrieved supplier with ID: {}", supplierId);
+            logger.info("Retrieved supplier with ID: {}", individualSupplierId);
             return new ResponseEntity<>(supplier.get(), HttpStatus.OK);
         } else {
-            logger.warn("Supplier with ID {} not found", supplierId);
+            logger.warn("Supplier with ID {} not found", individualSupplierId);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     // Update supplier by ID
     @PutMapping("/update/{supplierId}")
-    public ResponseEntity<SupplierDto> updateSupplier(@PathVariable Long supplierId, @RequestBody SupplierDto updatedSupplierDTO) {
-        SupplierDto updatedSupplier = supplierService.updateSupplier(supplierId, updatedSupplierDTO);
+    public ResponseEntity<IndividualSupplierDto> updateSupplier(@PathVariable Long individualSupplierId, @RequestBody IndividualSupplierDto updatedSupplierDTO) {
+        IndividualSupplierDto updatedSupplier = individualSupplierService.updateSupplier(individualSupplierId, updatedSupplierDTO);
         if (updatedSupplier != null) {
-            logger.info("Updated supplier with ID: {}", supplierId);
+            logger.info("Updated supplier with ID: {}", individualSupplierId);
             return new ResponseEntity<>(updatedSupplier, HttpStatus.OK);
         } else {
-            logger.warn("Supplier with ID {} not found for update", supplierId);
+            logger.warn("Supplier with ID {} not found for update", individualSupplierId);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
     
-    // Update supplier by ID
+    // Update supplier by Name
     @PutMapping("/update/{supplierName}")
-    public ResponseEntity<SupplierDto> updateSupplierName(@PathVariable String supplierName, @RequestBody SupplierDto updatedSupplierDTO) {
-        SupplierDto updatedSupplier = supplierService.updateSupplierName(supplierName, updatedSupplierDTO);
+    public ResponseEntity<IndividualSupplierDto> updateSupplierName(@PathVariable String supplierName, @RequestBody IndividualSupplierDto updatedSupplierDTO) {
+        IndividualSupplierDto updatedSupplier = individualSupplierService.updateSupplierName(supplierName, updatedSupplierDTO);
         if (updatedSupplier != null) {
             logger.info("Updated supplier with name: {}", supplierName);
             return new ResponseEntity<>(updatedSupplier, HttpStatus.OK);
@@ -89,16 +96,16 @@ public class SupplierController {
 
     // Delete supplier by ID
     @DeleteMapping("/delete/{supplierId}")
-    public ResponseEntity<Void> deleteSupplier(@PathVariable Long supplierId) {
-        supplierService.deleteSupplier(supplierId);
-        logger.info("Deleted supplier with ID: {}", supplierId);
+    public ResponseEntity<Void> deleteSupplier(@PathVariable Long individualSupplierId) {
+        individualSupplierService.deleteSupplier(individualSupplierId);
+        logger.info("Deleted supplier with ID: {}", individualSupplierId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     // Get suppliers by business owner name
     @GetMapping("/byBusinessOwner/{supplierName}")
-    public ResponseEntity<List<SupplierDto>> getSuppliersByBusinessOwnerName(@PathVariable String supplierName) {
-        List<SupplierDto> suppliers = supplierService.getSuppliersBySuppliername(supplierName);
+    public ResponseEntity<List<IndividualSupplierDto>> getSuppliersByBusinessOwnerName(@PathVariable String supplierName) {
+        List<IndividualSupplierDto> suppliers = individualSupplierService.getSuppliersBySuppliername(supplierName);
         if (!suppliers.isEmpty()) {
             logger.info("Retrieved {} suppliers with business owner name: {}", suppliers.size(), supplierName);
             return new ResponseEntity<>(suppliers, HttpStatus.OK);
@@ -111,6 +118,67 @@ public class SupplierController {
 	    @GetMapping("/count/supplier")
 	    public long countSupplier()
 	    {
-	    	return supplierService.countSupplier();
+	    	return individualSupplierService.countSupplier();
 	    }
+	//-------------------------------------------------------------------------------------------------    
+	    
+	  //API FOR INDIVIDUAL SUPPLIER   
+	 // Create a new supplier
+	    @PostMapping("/create/corporatesupplier")
+	    public ResponseEntity<CorporateSupplierDto> createCorporateSupplier(@RequestBody CorporateSupplierDto supplierDTO) {
+	        CorporateSupplierDto createdSupplier =corporateSupplierService.createCorporateSupplier(supplierDTO);
+	        logger.info("Created supplier with name: {}", createdSupplier.getSupplierName());
+	        return new ResponseEntity<>(createdSupplier, HttpStatus.CREATED);
+	    }
+
+	    // Get all suppliers
+	    
+	    @GetMapping("/get/corporatesuppliers")
+	    public ResponseEntity<List<CorporateSupplierDto>> getAllCorporateSuppliers() {
+	        List<CorporateSupplierDto> suppliers = corporateSupplierService.getAllCorporateSuppliers();
+	        logger.info("Retrieved {} suppliers from the database", suppliers.size());
+	        return new ResponseEntity<>(suppliers, HttpStatus.OK);
+	    }  
+	   
+	    // Get supplier by ID
+	    @GetMapping("/get/{corporateSupplierId}")
+	    public ResponseEntity<CorporateSupplierDto> getCorporateSupplierById(@PathVariable Long corporateSupplierId) {
+	        Optional<CorporateSupplierDto> supplier = corporateSupplierService.getCorporateSupplierById(corporateSupplierId);
+	        if (supplier.isPresent()) {
+	            logger.info("Retrieved supplier with ID: {}", corporateSupplierId);
+	            return new ResponseEntity<>(supplier.get(), HttpStatus.OK);
+	        } else {
+	            logger.warn("Supplier with ID {} not found", corporateSupplierId);
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
+	    }
+
+	    // Update supplier by ID
+	    @PutMapping("/update/{corporateSupplierId}")
+	    public ResponseEntity<CorporateSupplierDto> updateCorporateSupplier(@PathVariable Long corporateSupplierId, @RequestBody CorporateSupplierDto updatedSupplierDTO) {
+	        CorporateSupplierDto updatedSupplier = corporateSupplierService.updateCorporateSupplier(corporateSupplierId, updatedSupplierDTO);
+	        if (updatedSupplier != null) {
+	            logger.info("Updated supplier with ID: {}", corporateSupplierId);
+	            return new ResponseEntity<>(updatedSupplier, HttpStatus.OK);
+	        } else {
+	            logger.warn("Supplier with ID {} not found for update", corporateSupplierId);
+	            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	        }
+	    }
+	    
+	    // Delete supplier by ID
+	    @DeleteMapping("/delete/{corporateSupplierId}")
+	    public ResponseEntity<Void> deleteCorporateSupplier(@PathVariable Long corporateSupplierId) {
+	        individualSupplierService.deleteSupplier(corporateSupplierId);
+	        logger.info("Deleted supplier with ID: {}", corporateSupplierId);
+	        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    }
+	    
+	    //Count Corporate Supplier 
+	    @GetMapping("/count/corporatesupplier")
+	    public long countCorporateSupplier()
+	    {
+	    	return corporateSupplierService.countCorporateSupplier();
+	    }
+	    
 }
--- auth-module/src/main/java/com/orivesolutions/erp/exception/UserAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.orivesolutions.erp.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
